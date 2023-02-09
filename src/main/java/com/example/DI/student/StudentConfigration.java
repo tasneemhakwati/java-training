60ef9ea9044d863a10e27d9ae24b594f8b6e6c1b
tasneemhakwati <tasneemhakwati@gmail.com>
2023-02-09 12:23:18 +0200

    add student to student Table in db

---
+++ src/main/java/com/example/DI/student/StudentConfigration.java
@@ -0,0 +1,21 @@
+package com.example.DI.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+@Configuration
+public class StudentConfigration {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> studentRepository.save(new Student(
+                "Mariam",
+                "Mariam@Mariam.com",
+                LocalDate.of(2000, Month.APRIL, 12),
+                23));
+    }
+}
